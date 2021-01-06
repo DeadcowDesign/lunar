59940f64c85cead5052fbe9f9ddafa039c2075bb
James Filby <j.filby@derby.ac.uk>
2021-01-06 14:05:37 +0000

    Removed Twig from main lunar branch

--- dist/Application/Controller/Controller.php
+++ dist/Application/Controller/Controller.php
@@ -8,27 +8,8 @@ namespace Application\Controller;
  * initialisation). Uses a function called init which is called on construct.
  */
 class Controller extends \Core\Controller {
-
-	public $twig = null;
-
+	
     protected function init() {
-
-    	$this->bootTwig();
-    }
-
-    protected function bootTwig() {
-
-
-    	//Adding twig to test if easy.
-    	require_once BASE_PATH . '/Application/Libs/Twig/Autoloader.php';
-		\Twig_Autoloader::register(); 
-    	
-    	$loader = new \Twig_Loader_Filesystem(TWIG_TEMPLATE_PATH);
-		
-		$this->twig = new \Twig_Environment($loader, array(
-			'cache' => false, //TWIG_CACHE_PATH,
-		));
-
-		$this->twig->addGlobal('baseurl', BASE_URL);
-    }
+		return true;
+	}
 }
