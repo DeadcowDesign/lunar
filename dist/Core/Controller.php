00f5ebccba75ba423c64c3b00b872bdf038d2089
James Filby <j.filby@derby.ac.uk>
2021-01-06 14:26:34 +0000

    Adding correctly cased files

---
+++ dist/Core/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Core;
+
+class Controller
+{
+
+    public function __construct()
+    {
+
+        $this->init();
+    }
+
+    protected function init()
+    {
+        return true;
+    }
+}
